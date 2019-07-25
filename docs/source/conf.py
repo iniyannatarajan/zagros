c2e86caca470258012b36b1d4188a309463dcd33
Iniyan Natarajan <iniyannatarajan@gmail.com>
2019-07-25 19:05:02 +0200

    Modify project name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 
 # -- Project information -----------------------------------------------------
 
-project = u'radiobayes'
+project = u'RadioBayes'
 copyright = u'2019, Iniyan Natarajan'
 author = u'Iniyan Natarajan'
 
